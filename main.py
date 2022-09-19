765c9573e3c8f3779d106dae98326e708406da52
Manvan33 <rikostik@gmail.com>
2022-09-19 17:18:18 +0200

    Splitted into more functions

--- main.py
+++ main.py
@@ -15,7 +15,17 @@ def get_interfaces_with_tag(tag):
     return nb.dcim.interfaces.filter(tag=tag)
 
 
-def dhcp_reservation(mac, ip, hostname):
+def get_addresses_for_interface(id):
+    return nb.ipam.ip_addresses.filter(interface_id=id)
+
+
+def assign_ip_to_interface(ip, interface):
+    return nb.ipam.ip_addresses.create(address=ip.address, assigned_object_type="dcim.interface",
+                                       assigned_object_id=interface.id,
+                                       description=f"[auto-dhcp] {interface.device.name} ({interface.name})")
+
+
+def dhcp_reservation_line(mac, ip, hostname):
     return f"dhcp-host={mac},{hostname},{ip.address.split('/')[0]},12h"
 
 
@@ -25,15 +35,16 @@ def main():
     for interface in get_interfaces_with_tag(INTERFACES_TAG):
         if interface.count_ipaddresses > 0:
             # interface has already an IP address assigned
-            ip = next(nb.ipam.ip_addresses.filter(interface_id=interface.id))
+            ip = next(get_addresses_for_interface(interface.id))
             ip.tags = [{"name": INTERFACES_TAG, "slug": INTERFACES_TAG}]
             ip.save()
         else:
             # Find an available IP address
             ip = get_next_available_ip(PREFIX_ID)
-            nb.ipam.ip_addresses.create(address=ip, assigned_object_type="dcim.interface", assigned_object_id=interface.id,
-                                        description=f"[auto-dhcp] {interface.device.name} ({interface.name})", tags=[{"name": INTERFACES_TAG, "slug": INTERFACES_TAG}])
-        print(dhcp_reservation(interface.mac_address, ip, interface.device.name))
+            ip_object = assign_ip_to_interface(ip, interface)
+            ip_object.tags = [{"name": INTERFACES_TAG, "slug": INTERFACES_TAG}]
+            ip_object.save()
+        print(dhcp_reservation_line(interface.mac_address, ip, interface.device.name))
 
 
 if __name__ == '__main__':
