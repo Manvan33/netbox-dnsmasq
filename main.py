d260884954b105dc8a1e9b6e9d8d1c15868c3705
Manvan33 <rikostik@gmail.com>
2022-09-19 17:12:13 +0200

    Add a tag to the IP Address too

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ nb = pynetbox.api(
 
 
 def get_next_available_ip(prefix_id):
-    return nb.ipam.prefixes.get(prefix_id).available_ips.list()[0].address
+    return nb.ipam.prefixes.get(prefix_id).available_ips.list()[0]
 
 
 def get_interfaces_with_tag(tag):
@@ -16,7 +16,7 @@ def get_interfaces_with_tag(tag):
 
 
 def dhcp_reservation(mac, ip, hostname):
-    return f"dhcp-host={mac},{hostname},{ip.split('/')[0]},12h"
+    return f"dhcp-host={mac},{hostname},{ip.address.split('/')[0]},12h"
 
 
 def main():
@@ -25,12 +25,14 @@ def main():
     for interface in get_interfaces_with_tag(INTERFACES_TAG):
         if interface.count_ipaddresses > 0:
             # interface has already an IP address assigned
-            ip = next(nb.ipam.ip_addresses.filter(interface_id=interface.id)).address
+            ip = next(nb.ipam.ip_addresses.filter(interface_id=interface.id))
+            ip.tags = [{"name": INTERFACES_TAG, "slug": INTERFACES_TAG}]
+            ip.save()
         else:
             # Find an available IP address
             ip = get_next_available_ip(PREFIX_ID)
             nb.ipam.ip_addresses.create(address=ip, assigned_object_type="dcim.interface", assigned_object_id=interface.id,
-                                        description=f"[auto-dhcp] {interface.device.name} ({interface.name})", tag=INTERFACES_TAG)
+                                        description=f"[auto-dhcp] {interface.device.name} ({interface.name})", tags=[{"name": INTERFACES_TAG, "slug": INTERFACES_TAG}])
         print(dhcp_reservation(interface.mac_address, ip, interface.device.name))
 
 
